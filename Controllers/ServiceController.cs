77e9764ceacd54e317cb7da2b119fd1e85c9d02e
roopekt <roope.keto-tokoi@outlook.com>
2022-05-14 12:49:25 +0300

    steps 8-9
    - Http commands: add-service, get-service, get-all-services
    - ServiceController.Services is now a static field, so that it's life-time is the life-time of the whole server

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -7,21 +7,39 @@ namespace Task1.Controllers
     [Route("[controller]")]
     public class ServiceController : ControllerBase
     {
-        public List<Service> Services;
+        public static List<Service> Services = new List<Service> {
+            new Service(
+                "0",
+                new DateTime(2000, 3, 4),
+                new Device("0000", "S22", "Samsung")
+            )
+        };
 
         private readonly ILogger<ServiceController> _logger;
 
         public ServiceController(ILogger<ServiceController> logger)
         {
             _logger = logger;
+        }
 
-            Services = new List<Service> {
-                new Service(
-                    "0",
-                    new DateTime(2000, 3, 4),
-                    new Device("0000", "S22", "Samsung")
-                )
-            };
+        [HttpGet("get-service")]
+        public Service GetService(string id)
+        {
+            #pragma warning disable CS8603 // Possible null reference return.
+            return Services.FirstOrDefault(x => x.Id == id);
+            #pragma warning restore CS8603
+        }
+
+        [HttpGet("get-all-services")]
+        public IEnumerable<Service> GetAllServices()
+        {
+            return Services;
+        }
+
+        [HttpPost("add-service")]
+        public void AddService(Service service)
+        {
+            Services.Add(service);
         }
     }
 }
